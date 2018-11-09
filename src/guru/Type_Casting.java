66bc98442f7a7c8f22969da93de29c32f16dc96b
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-11-09 14:09:12 +0700

    cm

--- src/guru/Type_Casting.java
+++ src/guru/Type_Casting.java
@@ -14,14 +14,20 @@ public class Type_Casting {
 		byte x;
 		int a = 270;
 		double b = 128.128;
-		System.out.println("int converted to byte");
+		
+		System.out.println("int converted to byte:");
 		x = (byte) a;
 		System.out.println("a and x " + a + " " + x);
-		System.out.println("double converted to int");
+		System.out.println("----------------------");
+		
+		System.out.println("\ndouble converted to int:");
 		a = (int) b;
 		System.out.println("b and a " + b + " " + a);
-		System.out.println("\ndouble converted to byte");
+		System.out.println("----------------------");
+		
+		System.out.println("\ndouble converted to byte:");
 		x = (byte) b;
 		System.out.println("b and x " + b + " " + x);
+		System.out.println("----------------------");
 	}
 }
