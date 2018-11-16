d8b63b0bad57ff1d15b272f95dfedae2df64fee7
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-11-16 07:45:29 +0700

    cm

--- src/guru/String_Sample.java
+++ src/guru/String_Sample.java
@@ -12,13 +12,31 @@ public class String_Sample {
 		// String Concatenation
 		String str1 = "Rock";
 		String str2 = "Star";
-		
+
 		// Method 1 : Using concat
 		String str3 = str1.concat(str2);
 		System.out.println(str3);
-		
+		System.out.println("Length of String: " + str3.length());
+		System.out.println("Character at position 5: " + str3.charAt(5));
+		// Index of a given character
+		System.out.println("Index of character 'S': " + str3.indexOf('R'));
+
 		// Method 2 : Using "+" operator
 		String str4 = str1 + str2;
-		System.out.println(str4);
+		System.out.println(str4 + ", Length of String: " + str4.length());
+
+		// Compare to
+		System.out.println("Compare To 'ROCKSTAR': " + str3.compareTo("rockstar"));
+		// Compare to - Ignore case
+		System.out.println("Compare To 'ROCKSTAR' - Case Ignored: " + str3.compareToIgnoreCase(str4));
+
+		// Contain method
+		System.out.println("Contains sequence 'tar': " + str3.contains("tars"));
+
+		// Ends With
+		System.out.println("EndsWith character 'r': " + str3.endsWith("r"));
+
+		// "replaceAll" & "replaceFirst"
+		System.out.println("Replace 'Rock' with 'Duke': " + str3.replace("Rock", "Duke"));
 	}
 }
